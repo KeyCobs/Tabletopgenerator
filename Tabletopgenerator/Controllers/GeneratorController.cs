8d18bd6766a33a0e7763d3252f91ada11aa55b8b
Keycobs <kevinjacobs@live.nl>
2025-08-03 16:42:10 +0200

    have page template

---
+++ Tabletopgenerator/Controllers/GeneratorController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tabletopgenerator.Repository.Implementation;
+
+namespace Tabletopgenerator.Controllers
+{
+    public class GeneratorController : Controller
+    {
+        private readonly INameGeneratorRepository _nameGeneratorRepository;
+
+        public GeneratorController(INameGeneratorRepository nameGeneratorRepository)
+        {
+            _nameGeneratorRepository = nameGeneratorRepository;
+        }
+
+        public IActionResult CharacterGenerator()
+        {
+            return View();
+        }
+    }
+}
